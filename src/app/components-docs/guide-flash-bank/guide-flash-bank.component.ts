b8114f9e1065745652b1997767636b0f13c3330b
Dieuleveut-Ngele <dieuleveutngele@gmail.com>
2024-08-30 16:06:15 +0100

    heignt commit

--- src/app/components-docs/guide-flash-bank/guide-flash-bank.component.ts
+++ src/app/components-docs/guide-flash-bank/guide-flash-bank.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-guide-flash-bank',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
   templateUrl: './guide-flash-bank.component.html',
   styleUrl: './guide-flash-bank.component.scss'
 })
